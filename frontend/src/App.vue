491653e02da753aafa08b6293143225476de9f03
Sobithan Jegatheeswaran <sobithan2005@gmail.com>
2026-09-09 20:57:57 +0100

    Initial project setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h1>Renovation Budget Simulator</h1>
+  </main>
+</template>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+
+main {
+  padding: 40px;
+}
+</style>
